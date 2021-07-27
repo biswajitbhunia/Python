cafb56b12e6b5d70c63ecccfd72cc19ec622d51a
biswajitbhunia <44657622+biswajitbhunia@users.noreply.github.com>
2021-07-27 07:41:12 +0530

    Update Prime3.py

--- Prime3.py
+++ Prime3.py
@@ -1,11 +1,11 @@
-n=input('enter a number')
+n=eval(input('enter a number'))
 m=int(n/2)
 for i in range(2,m):
      r=n%i
-if r==0:
-break
+     if r==0:
+          break
 if r!=0:
-print('prime')
-else
-print('not prime')
+     print('prime')
+else:
+     print('not prime')
 
